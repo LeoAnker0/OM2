5988004386c1f0d9e374c990e5502aa33c64d369
Leo <lanker013@gmail.com>
2023-05-14 21:03:38 +0100

    have now setup the database so that it has data storage which is permaneant, as well as installing a pgadmin container, and then also figuring out how to connect the fastapi to the postgres database music_streaming
    next steps are figuring out a real plan for creating the signup/login pages securely, learning all about the proper ways of doing authorisation/authentication, getting that working, and then focusing on the core code for the music streaming aspect (and all the new things we wanted to add)

--- src/new/apis/app/main.py
+++ src/new/apis/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, Header, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import asyncpg
+import os
 
 from typing import Optional
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -14,9 +19,43 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+async def create_db_pool():
+    # Modify the connection parameters as per your PostgreSQL container configuration
+    pool = await asyncpg.create_pool(
+        host="postgres",  # Hostname or IP address of your PostgreSQL container
+        port=5432,  # Port number of your PostgreSQL container
+        user=str(os.environ.get("POSTGRES_USER")),  # Username to connect to the PostgreSQL container
+        password=str(os.environ.get("POSTGRES_PASSWORD")),  # Password to connect to the PostgreSQL container
+        database=str(os.environ.get("POSTGRES_DB")),  # Database name
+    )
+    return pool
+
+
+@app.on_event("startup")
+async def startup():
+    app.state.pool = await create_db_pool()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await app.state.pool.close()
+
 @app.get("/")
 async def read_root():
-	return {"hello": "world"}
+	 # Get the database connection pool from the app state
+    pool = app.state.pool
+
+    # Fetch all table names from the database
+    async with pool.acquire() as connection:
+        table_names = await connection.fetch("""
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema = 'public'
+        """)
+
+    # Extract the table names from the result
+    tables = [table["table_name"] for table in table_names]
+
+    return {"tables": tables}
 
 @app.get("/items/{item_id}")
 async def read_items(item_id: int, q: Optional[str] = None, version: str = Header(...)):
@@ -26,3 +65,7 @@ async def read_items(item_id: int, q: Optional[str] = None, version: str = Heade
         raise HTTPException(status_code=400, detail="Invalid version provided")
 
 #localhost 8000
+
+
+
+
