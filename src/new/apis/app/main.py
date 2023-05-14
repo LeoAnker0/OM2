cc7f592abdea9d78c21ecb0425d5a547cd04b9f9
Leo <lanker013@gmail.com>
2023-05-14 17:23:20 +0100

    changed the docker compose so that it always restarts our code, as well as changing main.py so that it has versioning

--- src/new/apis/app/main.py
+++ src/new/apis/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 from typing import Optional
@@ -19,7 +19,10 @@ async def read_root():
 	return {"hello": "world"}
 
 @app.get("/items/{item_id}")
-async def read_items(item_id: int, q: Optional[str] = None):
-	return {"item_id": item_id, "q": q}
+async def read_items(item_id: int, q: Optional[str] = None, version: str = Header(...)):
+    if version == "1":
+        return {"item_id": item_id, "q": q, "version": "1"}
+    else:
+        raise HTTPException(status_code=400, detail="Invalid version provided")
 
 #localhost 8000
