8d72b34d1d3fc8c3eaa93715849accd3fb131bd3
Leo <lanker013@gmail.com>
2023-06-06 12:52:53 +0100

    have gotten all parts of the search bar working, with the focus and fixed a bug where you weren't able to click on the clear icon, because it went invisible, so instead i changed to using opacity for that part, and it appears to be working perfectly

--- src/new/web/Orange-Music-2/js/search.js
+++ src/new/web/Orange-Music-2/js/search.js
@@ -7,18 +7,18 @@ export function setEventListenersForSearchbar() {
     /* detects if the searchbar has been typed in */
     searchBar.addEventListener("keyup", () => {
         if (searchBar.value && clearIcon.style.visibility != "visible") {
-            clearIcon.style.visibility = "visible";
+            clearIcon.style.opacity = "100%";
         } else if (!searchBar.value) {
-            clearIcon.style.visibility = "hidden";
+            clearIcon.style.opacity = "0%";
         }
     });
 
     /* checks if the clear icon is clicked, and then hides it */
     clearIcon.addEventListener("click", () => {
         searchBar.value = "";
-        clearIcon.style.visibility = "hidden";
+        clearIcon.style.opacity = "0%";
 
-        console.log("clear icon clicked")
+        //console.log("clear icon clicked")
     })
 
     /* detects when the enter key is pressed, and logs the search query */
@@ -33,6 +33,27 @@ export function setEventListenersForSearchbar() {
         }
     });
 
-    /* detect when being focusued, rather than typed */
+    const outlineForSearchbar = document.querySelector('.topleft-searchbarContainerL2');
 
-}
+    /* detect when being focusued, rather than typed */
+    searchBar.addEventListener('focus', () => {
+        outlineForSearchbar.classList.remove('default');
+        outlineForSearchbar.classList.add('updated')
+
+        /* if searchBar.value is greater than 0*/
+        if (searchBar.value.length > 0) {
+            clearIcon.style.opacity = "100%";
+
+        }
+    });
+
+    searchBar.addEventListener('blur', () => {
+        outlineForSearchbar.classList.remove('updated');
+        outlineForSearchbar.classList.add('default')
+
+        /* hide the clear icon (we changed over to using opacity so that it was always clickable) */
+        clearIcon.style.opacity = "0%";
+
+    });
+
+};
