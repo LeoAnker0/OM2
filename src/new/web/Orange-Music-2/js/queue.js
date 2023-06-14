d1a28d33083f187d46e01d2794c81aa2bd9fd61c
Leo <lanker013@gmail.com>
2023-06-14 15:49:30 +0100

    drag and drop ordering of the queue items is now working perfectly, and it was surprisingly pain free, like surprisingly easy, all thanks to that drag and drop api

--- src/new/web/Orange-Music-2/js/queue.js
+++ src/new/web/Orange-Music-2/js/queue.js
@@ -10,7 +10,7 @@ export function initialiseQueue() {
 
     /* load in the html for loading in the queue */
 
-    hydrateQueue();
+    updateQueue();
     return
 }
 
@@ -68,18 +68,20 @@ import queueItem from '../html/queueItem.html?raw';
 we want the only export here to be the update function
  */
 
-const songs = [
+let songs = [
     { id: 1, name: "Changes are over", artist: "I messed up", duration: "3:45", coverImage: "https://plus.unsplash.com/premium_photo-1677087121676-2acaaae5b3c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c3F1YXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" },
     { id: 2, name: "ARIES", artist: "lives", duration: "4:20", coverImage: "https://images.unsplash.com/photo-1605106901227-991bd663255c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3F1YXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" },
     { id: 3, name: "A little place called the moon", artist: "Aurora", duration: "2:55", coverImage: "https://images.unsplash.com/photo-1603208636124-a12f78c1bb6c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c3F1YXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" }
 ];
 
-function hydrateQueue() {
+function updateQueue() {
     const queueContainer = document.getElementById("QUEUEbottomContainer");
 
     queueContainer.innerHTML = "";
 
     for (let i = 0; i < songs.length; i++) {
+        songs[i].id = i;
+
         const song = songs[i];
         const html = hydrateItem(song);
         const div = document.createElement('div');
@@ -93,8 +95,83 @@ function hydrateQueue() {
         });
         div.dataset.songId = song.id;
 
+        // Add click event listener to handle queue item click
+        div.addEventListener('click', handleQueueItemClick);
+        div.dataset.songId = song.id;
+
+
+        // Find the "QUEUE-item-image-removeIndicator" element and attach a click event listener
+        const removeIndicator = div.querySelector('.QUEUE-item-image-removeIndicator');
+        removeIndicator.addEventListener('click', handleQueueItemRemove);
+        removeIndicator.dataset.songID = song.id;
+
+        // Find the "QUEUE-item-up-next-queue-item-context-call" element and attach a click event listener
+        const menuPopupIcon = div.querySelector('.QUEUE-item-up-next-queue-item-context-call');
+        menuPopupIcon.addEventListener('click', handleQueueDisplayMenu);
+        menuPopupIcon.dataset.songID = song.id;
+
+        // Add dragstart event listener for starting the drag
+        div.addEventListener("dragstart", handleDragStart);
+
+        // Add dragover event listener for allowing drop
+        div.addEventListener("dragover", handleDragOver);
+
+        // Add drop event listener for handling the drop
+        div.addEventListener("drop", handleDrop);
+
+
         queueContainer.appendChild(div);
     }
+}
+
+function handleDragStart(event) {
+    const dragSource = event.target;
+    event.dataTransfer.setData("text/plain", dragSource.dataset.songId);
+}
+
+function handleDragOver(event) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+}
+
+function handleDrop(event) {
+    event.preventDefault();
+    const dragSourceId = event.dataTransfer.getData("text/plain");
+    const dropTarget = event.target;
+
+    // Find the corresponding song objects
+    const dragSourceSong = songs.find(song => song.id === parseInt(dragSourceId));
+    const dropTargetSong = songs.find(song => song.id === parseInt(dropTarget.dataset.songId));
+
+    // Swap the positions of the songs in the array
+    const dragSourceIndex = songs.indexOf(dragSourceSong);
+    const dropTargetIndex = songs.indexOf(dropTargetSong);
+    [songs[dragSourceIndex], songs[dropTargetIndex]] = [songs[dropTargetIndex], songs[dragSourceIndex]];
+
+    // Re-render the queue to reflect the new order
+    updateQueue();
+
+    console.log(songs);
+}
+
+function handleQueueItemRemove(event) {
+    event.stopPropagation();
+    const clickedItem = event.target;
+    const songID = parseInt(clickedItem.dataset.songID);
+
+    songs = songs.filter(item => item.id !== songID);
+
+    console.log('Clicked remove indicator for song ID:', songID);
+    updateQueue();
+}
+
+function handleQueueDisplayMenu(event) {
+    event.stopPropagation();
+    const clickedItem = event.target;
+    const songId = clickedItem.dataset.songId;
+    console.log('Clicked show menu:', songId);
+
+    /* call an imported function */
 }
 
 function handleQueueItemClick(songId) {
@@ -143,9 +220,15 @@ function hydrateItem(song) {
     }
     replacedContent = replacedContent.replace('<div', `<div data-song-id="${song.id}"`);
 
+    // Find the "QUEUE-item-image-removeIndicator" element and attach a click event listener
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = replacedContent;
+    const removeIndicator = tempElement.querySelector('.QUEUE-item-image-removeIndicator');
+    removeIndicator.addEventListener('click', function() {
+        console.log('Clicked remove indicator for song ID:', song.id);
+    });
 
-
-    return replacedContent;
+    return tempElement.innerHTML;
 }
 
 
