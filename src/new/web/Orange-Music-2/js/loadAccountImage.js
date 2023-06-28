7188c0077b70d9cb69500fb67f4753cd2fd7e3f6
Leo <lanker013@gmail.com>
2023-06-28 21:29:27 +0100

    have now got the menu connected properly, but of course since we don't have any backend code, none of this stuff here can actually real do much of anything

--- src/new/web/Orange-Music-2/js/loadAccountImage.js
+++ src/new/web/Orange-Music-2/js/loadAccountImage.js
@@ -1,9 +1,40 @@
 export function initAccountImg() {
+    /* loading in the img */
     const accountImgSource = document.getElementById("TOPRIGHTaccountImg");
-    accountImgSource.src = "https://images.unsplash.com/photo-1605106901227-991bd663255c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3F1YXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60"
+    const newImage = "https://images.unsplash.com/photo-1605106901227-991bd663255c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3F1YXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60";
+    accountImgSource.src = newImage;
 
+    /* adding event listeners for loading in the menu for the account menu */
+    const accountImgButton = document.getElementById("TOPRIGHTaccountImgButton");
+    accountImgButton.addEventListener('click', handleClick);
+}
 
-    console.log("account img");
+function handleClick(event) {
+    event.stopPropagation;
+    console.log("handleClick");
+    handleQueueDisplayMenu(event)
 
+    return
+}
 
+import { MENUdisplay } from './menu.js';
+
+function handleQueueDisplayMenu(event) {
+
+    const params = [{
+        displayText: 'Help',
+        optionalSVG: 'icons_helpIcon',
+        function: 'None'
+    }, {
+        displayText: 'Settings',
+        optionalSVG: 'icons_derpy',
+        function: 'None',
+    }, {
+        displayText: 'Sign Out',
+        optionalSVG: 'None',
+        function: 'None',
+    }]
+
+    MENUdisplay(params, event);
+    return;
 }
