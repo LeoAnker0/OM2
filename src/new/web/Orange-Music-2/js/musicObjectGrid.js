52bc06551b94b6a8627362f7f9d792e48df4b968
Leo <lanker013@gmail.com>
2023-06-29 19:06:46 +0100

    MOG items
    So i ran into some good luck, in that the code that i made for the text last time was solid, so that means that i have basically just been able to copy paste it in, I will of couse need to work on how to make it fit my object fully, and i might even add in a little cheeky date indicator/last checked indicator

--- src/new/web/Orange-Music-2/js/musicObjectGrid.js
+++ src/new/web/Orange-Music-2/js/musicObjectGrid.js
@@ -38,9 +38,12 @@ function loadObjects() {
 
     for (var i = 0; i <= loadEvents - 1; i++) {
         let replacedContent = musicObjetsGridItem
-        const listOfThings = ['MOG_image', 'somethingElse'];
+        const listOfThings = ['MOG_image', 'MOG_text1', 'MOG_text2'];
 
         const imgAddress = "https://images.unsplash.com/photo-1605106901227-991bd663255c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3F1YXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60";
+        const textTop = "top text top text top ext hello there more and cake and then i ate a all da spaghetti";
+        const textBottom = "bottom text, there shouldn't be that much of this";
+
         for (let i = 0; i < listOfThings.length; i++) {
             const placeholder = listOfThings[i].toString();
             const regex = new RegExp(`\\{${placeholder}\\}`, 'g');
@@ -48,9 +51,12 @@ function loadObjects() {
 
             if (placeholder === 'MOG_image') {
                 value = imgAddress
-            } else if (placeholder === 'LCD_bottomText_placeholder') {
-                value = "Another thing to change and nroie and john the ripper";
+            } else if (placeholder === 'MOG_text1') {
+                value = textTop;
+            } else if (placeholder === 'MOG_text2') {
+                value = textBottom;
             }
+
 
 
             replacedContent = replacedContent.replace(regex, value);
