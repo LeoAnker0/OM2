4efcb2e1007ab82928097e88bbe2c1199b85c2a8
Leo <lanker013@gmail.com>
2023-06-29 18:57:04 +0100

    MOG items
    I got the image added in to the grid items, and now need to add the text and the buttons

--- src/new/web/Orange-Music-2/js/musicObjectGrid.js
+++ src/new/web/Orange-Music-2/js/musicObjectGrid.js
@@ -31,14 +31,30 @@ function loadInContainer() {
 import musicObjetsGridItem from '../html/musicObjectsGridItem.html?raw';
 
 function loadObjects() {
-    const loadEvents = 5;
+    const loadEvents = 15;
     const parentContainer = document.getElementById("MOGgridContainer");
 
     parentContainer.innerHTML = "";
 
     for (var i = 0; i <= loadEvents - 1; i++) {
-        parentContainer.innerHTML += musicObjetsGridItem;
+        let replacedContent = musicObjetsGridItem
+        const listOfThings = ['MOG_image', 'somethingElse'];
 
+        const imgAddress = "https://images.unsplash.com/photo-1605106901227-991bd663255c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3F1YXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60";
+        for (let i = 0; i < listOfThings.length; i++) {
+            const placeholder = listOfThings[i].toString();
+            const regex = new RegExp(`\\{${placeholder}\\}`, 'g');
+            let value = '';
+
+            if (placeholder === 'MOG_image') {
+                value = imgAddress
+            } else if (placeholder === 'LCD_bottomText_placeholder') {
+                value = "Another thing to change and nroie and john the ripper";
+            }
+
+
+            replacedContent = replacedContent.replace(regex, value);
+        }
+        parentContainer.innerHTML += replacedContent;
     }
-
 }
