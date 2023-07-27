238ec72d3637021bc64773b5d37cff316b4f1570
Leo <lanker013@gmail.com>
2023-07-27 17:08:49 +0100

    UPDATE signup
    This is a big milestone, i have finally figured out how to get data backwards and forwards, and at this point in time i have managed to add a new user to the database, and currently a few of the details are perhaps a bit on the false side, howevever now that i can see it in pgadmin it won't take long to create real data that will be what it should actually be.
    
    This has taken most of today to get to the point where fast api, front end javascript and postgres are all talking together properly and i think this is a good time to take a little pause, since this is a great point to pick up from tomorrow, since i now know exactly how to get my data back and forth

--- src/apis/app/main.py
+++ src/apis/app/main.py
@@ -1,14 +1,28 @@
-from fastapi import FastAPI, Header, HTTPException
+from fastapi import FastAPI, Header, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+import json
+
+
 import asyncpg
 import os
-
-from typing import Optional
+import secrets
 
 load_dotenv()
 
 app = FastAPI()
+
+# Temporary data store to store form data with tokens
+signup_data_store = {}
+
+class SignupData(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    profile_picture: str  # You can store the image URL in the database
+
 
 # Configure CORS
 app.add_middleware(
@@ -19,14 +33,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 async def create_db_pool():
-    # Modify the connection parameters as per your PostgreSQL container configuration
     pool = await asyncpg.create_pool(
-        host="postgres",  # Hostname or IP address of your PostgreSQL container
-        port=5432,  # Port number of your PostgreSQL container
-        user=str(os.environ.get("POSTGRES_USER")),  # Username to connect to the PostgreSQL container
-        password=str(os.environ.get("POSTGRES_PASSWORD")),  # Password to connect to the PostgreSQL container
-        database=str(os.environ.get("POSTGRES_DB")),  # Database name
+        database=str(os.environ.get("POSTGRES_DB")),
+        user=str(os.environ.get("POSTGRES_USER")),
+        password=str(os.environ.get("POSTGRES_PASSWORD")),
+        host="postgres",  # Replace with your actual database host
+        port="5432",      # Replace with your actual database port
     )
     return pool
 
@@ -39,33 +53,59 @@ async def startup():
 async def shutdown():
     await app.state.pool.close()
 
-@app.get("/")
-async def read_root():
-	 # Get the database connection pool from the app state
-    pool = app.state.pool
 
-    # Fetch all table names from the database
-    async with pool.acquire() as connection:
-        table_names = await connection.fetch("""
-            SELECT table_name
-            FROM information_schema.tables
-            WHERE table_schema = 'public'
-        """)
+async def is_email_unique(email):
+    async with app.state.pool.acquire() as conn:
+        query = "SELECT COUNT(*) FROM users WHERE email = $1"
+        result = await conn.fetchval(query, email)
+        return result == 0
 
-    # Extract the table names from the result
-    tables = [table["table_name"] for table in table_names]
+async def insert_user(data: dict):
+    async with app.state.pool.acquire() as conn:
+        query = "INSERT INTO users (username, email, password, profile_picture, uuid, description, date_joined) VALUES ($1, $2, $3, $4, $5, $6, $7)"
+        await conn.execute(query, data["username"], data["email"], data["password"], data["profile_picture"], "3", "oui", "today")
 
-    return {"tables": tables}
+@app.post("/apis/signup/")
+async def signup(request: Request):
 
-@app.get("/items/{item_id}")
-async def read_items(item_id: int, q: Optional[str] = None, version: str = Header(...)):
-    if version == "1":
-        return {"item_id": item_id, "q": q, "version": "1"}
-    else:
-        raise HTTPException(status_code=400, detail="Invalid version provided")
+    raw_data = await request.body()
+    data = json.loads(raw_data)
 
-#localhost 8000
+    email_unique = await is_email_unique(data["email"])
+
+    if not email_unique:
+        raise HTTPException(status_code=400, detail="Email already exists")
+
+    # Step 2: Generate a unique token and store the form data in the temporary data store
+    token = secrets.token_urlsafe(16)
+    signup_data_store[token] = data["email"]
+
+    # Return the token to the client-side
+    return {"status": "email_validated", "token": token}
+
+@app.post("/apis/signup/complete/")
+async def complete_signup(request: Request):
+    # Retrieve the form data from the temporary data store using the token
+    raw_data = await request.body()
+    data = json.loads(raw_data);
+    token = data["token"]
+    username = data["username"]
+    password = data["password"]
+    email = data["email"]
+    profilePicture = data["profilePicture"]
+
+    print(f"token: {token} \t| username: {username} \t| password: {password} \t| email: {email}")
+
+    signup_data = signup_data_store.get(token)
+
+    if not signup_data:
+        raise HTTPException(status_code=400, detail="Invalid token")
+
+    del signup_data_store[token]
+   
+    #add user to the db
+    user_dict = dict(username=username, password=password, email=email, profile_picture="cheese")
+    await insert_user(user_dict)
 
 
-
-
+    return {"message": "Signup successful"}
