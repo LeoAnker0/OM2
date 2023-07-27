0bae9f0c8b9d99d6770f341a696e7b13e7ea2d5b
Leo <lanker013@gmail.com>
2023-07-27 21:25:27 +0100

    UDATE signup
    Now have it setup so that the users password is hashed, and a UUID is generated, and the date is set correctdly, and right now the next steps are adding proper feedback for the user who is signing up that things are progressing and working properly, and setting up the system that will handle files, since we need to be able to restrict access to only those who should have access, as well as setting up tabels that know of all the files existence, and other systems that compress and otherwise deal with the images

--- src/apis/app/main.py
+++ src/apis/app/main.py
@@ -4,11 +4,12 @@ from dotenv import load_dotenv
 from typing import Optional
 from pydantic import BaseModel, EmailStr
 import json
-
-
 import asyncpg
 import os
 import secrets
+import uuid
+import datetime
+import bcrypt
 
 load_dotenv()
 
@@ -44,6 +45,14 @@ async def create_db_pool():
     )
     return pool
 
+def hash_password(password: str) -> str:
+    # Generate a salt and hash the password
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+
+    # The hashed_password is a bytes object, so convert it to a string before returning
+    return hashed_password.decode("utf-8")
+
 
 @app.on_event("startup")
 async def startup():
@@ -60,10 +69,27 @@ async def is_email_unique(email):
         result = await conn.fetchval(query, email)
         return result == 0
 
+
+
 async def insert_user(data: dict):
     async with app.state.pool.acquire() as conn:
+        data["description"] = "empty..."
+        data["date_joined"] = str(int(datetime.datetime.now().timestamp() * 1000))
+
+        while True:
+            # Generate a UUID
+            data["uuid"] = str(uuid.uuid4())
+
+            # Check if the UUID exists in the database
+            query = "SELECT COUNT(*) FROM users WHERE uuid = $1"
+            result = await conn.fetchval(query, data["uuid"])
+            if result == 0:
+                # Unique UUID found, break out of the loop
+                break
+
+
         query = "INSERT INTO users (username, email, password, profile_picture, uuid, description, date_joined) VALUES ($1, $2, $3, $4, $5, $6, $7)"
-        await conn.execute(query, data["username"], data["email"], data["password"], data["profile_picture"], "3", "oui", "today")
+        await conn.execute(query, data["username"], data["email"], data["password"], data["profile_picture"], data["uuid"], data["description"], data["date_joined"])
 
 @app.post("/apis/signup/")
 async def signup(request: Request):
@@ -90,11 +116,11 @@ async def complete_signup(request: Request):
     data = json.loads(raw_data);
     token = data["token"]
     username = data["username"]
-    password = data["password"]
+    password = hash_password(data["password"])
     email = data["email"]
     profilePicture = data["profilePicture"]
 
-    print(f"token: {token} \t| username: {username} \t| password: {password} \t| email: {email}")
+    #print(f"token: {token} \t| username: {username} \t| password: {password} \t| email: {email}")
 
     signup_data = signup_data_store.get(token)
 
