5223461787522ee29cd42eff2909b9e90160c743
Leo <lanker013@gmail.com>
2023-08-15 21:41:01 +0100

    UPDATE project view
    Have now implemented the x close to be a button with img svg, but i don't have the right svg at the moment, which is unfortunate.
    Also changed it so that the title and artist name and year and such were coming in as regex replacements, instead of hard coded.
    And have started working on the system whereby the user can edit the description using markdown

--- src/web/Orange-Music-2/js/projectView.js
+++ src/web/Orange-Music-2/js/projectView.js
@@ -1,11 +1,36 @@
 export function initProjectView() {
     loadContainer();
 
-    const description = `In this example, the .content-container holds the fading content, and the .fade-text element inside it has a mask that uses a linear gradient to control the opacity. The gradient is set up so that the content's opacity gradually changes from left to right.
-                    You can adjust the gradient stops, colors, and sizes to achieve the exact fading effect you're looking for. Keep in mind that browser compatibility for CSS masks might vary, so it's a good idea to test this approach across different browsers.`;
+    const description = `When Anne-Marie sought out therapy during the pandemic, the Essex-born singer-songwriter found it so transformative that she named her second album, 2021’s <i>Therapy</i>, after it. But then she began to feel a little <i>too</i> steady. “I got to the second album and I was like, ‘Wow, I’m really calm and collected and I know myself,’” she tells Apple Music. “But it got so boring. I want to be crazy and I want to feel really sad and I want to feel really angry. I think that’s why <i>UNHEALTHY</i> made so much sense to me—because I am, and my flaws are, and I’m all of these things that I didn’t feel I should’ve been.”
+                    On <i>UNHEALTHY</i>—which tells the story of Anne-Marie Nicholson moving from a turbulent relationship into a loving new one—the pop star doesn’t so much accept those flaws as revel in them (see “GRUDGE”, on which she brilliantly rejects being the bigger person). But the album is also an act of rebellion: against anyone telling Anne-Marie what to do, against the often sanitised image of female pop stardom (compare the ultra-polished cover of <i>Therapy</i> to that of <i>UNHEALTHY</i>), and against any expectations of her. “I [wanted] to test the water and burst people’s ideas of what I would be like,” adds Anne-Marie, who here swings between pop-rock (“HAUNT YOU”), country (the title track, assisted by Shania Twain), lullaby-like acoustic guitars (“KILLS ME TO LOVE YOU”) and spacious ballads (“NEVER LOVED ANYONE BEFORE”), with hints of “Mambo No. 5”, <i>Oliver!</i>, <i>reputation</i>-era Taylor Swift and plenty of tongue-in-cheek lyricism en route.
+                    Perhaps the real premise of <i>UNHEALTHY</i>, though, is that Anne-Marie could only have written it by reaching her happiest, healthiest state yet. “It was like, ‘Let me lose myself again, but still have the skills there to deal with pain and all that stuff,’” she adds. “I think it’s so important for people to own their brain—whether it’s chaotic or collected. We have to go and lose ourselves to eventually find out who we really are.” Here, Anne-Marie delves deep into her third album—from the “chaotic” to the “collected”—and the power she found in exploring her most “unhealthy” tendencies.
+                    <b>The chaotic:</b>
+
+                    
+                    <b>At first, the word “unhealthy” was about food.</b>
+                    “I went to LA to do a session, and I couldn’t get this image out of my head: me eating burgers and smoking cigarettes—with grease dripping down my chin—and ice cream and donuts and everything bad for me. I’m definitely stubborn, but I have also been a people-pleaser. I got to a point where I thought, ‘Fuck it, I’m going to do whatever I want to do.’ And if it means eating burgers when I’m a bit chubby at the moment, then I’m going to fucking do it. The cover didn’t end up being that, but either way, I wanted it to show that I ain’t polished. I want to be a normal girl. The cover we chose made me feel like a child again, which was something I felt I lost on the last album. For that shoot, I thought, ‘What would a kid do when they see a car window? They push their face up against it.’ It’s the most childish thing I could find to do.”
+                    <b>Then “unhealthy” became about relationships.</b>
+                    “Food, OCD routines that you have, your own thoughts can all be unhealthy. But during that session in LA, we also landed on relationships. And the idea that they can be unhealthy when your friends and family aren’t 100 per cent in it, like, ‘No babe, they’re not the one. They’re not good enough for you. You can find someone better.’ And I just felt like people should do whatever they want to do—and if the person isn’t right for them, they’ll figure it out and learn from it. That’s what the title track with Shania Twain is all about.”
+                    <b>I can make relationships unhealthy, too.</b>
+                    “An unhealthy relationship also comes from, honestly, me being completely obsessed with someone. With ‘OBSESSED’, I wanted to go back to my musical theatre days and couldn’t get the <i>Oliver!</i> song ‘I’d Do Anything’ out of my head. We were in America and I walked into the studio and said I thought the chorus should be, ‘I’d do anything for you, boy, anything.’ <i>Oliver!</i> mustn’t have been big there because they were all like, ‘Wow, that’s <i>amazing</i>!’ There was definitely an owning thing [on this album]. On ‘CUCKOO’, it was about saying, ‘Before you get into this with me, I want to let you know exactly who I am. And if you stay, you stay, and if you don’t, fine.’ It felt powerful to get with someone and be your complete self right from the start. It was owning it and saying, ‘This is who I am.’”
+                    <b>Forgiveness isn’t for me.</b>
+                    “I’ve always wanted to write a song about getting your ex back, but in therapy, I was like, ‘No no, don’t write a song about that, you should just let it go.’ We came up with the line ‘My therapist said, “Keep calm and don’t react,”’ and the rest of ‘GRUDGE’ came. It felt fun. I was Googling the craziest stuff people have done to get their exes back. One person put clingfilm all around their ex’s car against a lamp post and I was like, ‘This is the best.’ I have tattoos on my body of what friends have taught me in life, and one on my shoulder said, ‘Forgiveness’, after my friend Beanie, who is so good at forgiving people. But I thought to myself, ‘I don’t want to do that. I don’t want to forgive people if they’ve mugged me off or gone too far.’ The tattoo is half-removed because I don’t want that in my life.”
+                    <b>My version of love is chaotic and crazy.</b>
+                    “Even the love songs on this album are very twisted. ‘KILLS ME TO LOVE YOU’ and ‘NEVER LOVED ANYONE BEFORE’ are weird titles to have on something about love. I could write a love song that’s just completely beautiful, and butterflies and all of that, but that’s just not what happens. ‘KILLS ME TO LOVE YOU’ is my version of love. It’s weirdly celebrating crazy love, because I was with someone before that had a different version of that, and it didn’t work.”
+                    <b>The collected:</b>
+                    <b>I’ve finally understood my worth.</b>
+                    “I went through a long period of time thinking that other artists just didn’t like my music and that was tough. And then the first session after I came out of therapy was [2021 track] ‘Our Song’ with Niall Horan. And it was like, ‘Wow, that just happened. And he wanted to do that with me.’ With ‘UNHEALTHY’, I just knew Shania Twain would be perfect for it. When she came back, I was amazed. Was it validating to have someone like her on one of my songs? Absolutely. She is such a big star to [women of my age group], and she’s been doing it for so long.”
+                    <b>And I wanted to take the power back.</b>
+                    “The song ‘PSYCHO’ was about saying, ‘Fuck you. Maybe I am a bit crazy, but I love it!’ But also: ‘I’ve done some crazy stuff in my past but I definitely didn’t deserve that.’ The piano reminded me of <i>The Pink Panther</i> theme tune—like it was creeping up on something. It was cool and understated but suspenseful. I think we all felt catharsis when we made that song. It was late at night and we were all just lying on the floor, feeling free.”
+                    <b>I found a love I didn’t think I would ever have.</b>
+                    “I wrote ‘YOU &amp; I’ [featuring Khalid] to show people where I am in the relationship now. And I needed it to be the end of the album, because I didn’t want people to think I was <i>actually</i> nuts! It’s just a really beautiful song, and I’m so glad that Khalid did it as well, because his voice just fit perfectly. When I listen to that song, it makes me emotional. Because I don’t think I ever thought that I would find love like it. When you find that person, nothing else matters. Everything is OK and every element of life is easier.”
+                    <b>The music on this album makes more sense because I make more sense to myself.</b>
+                    “I’ve tried to be myself as much as I can be on every record. But on my first, I didn’t really know who I was as an artist, if I’m being honest. I was just making music. The second was about being really honest about what I’d learned in therapy. But it seems the third album is when I’ve actually worked out who I am as a human. I’ve got to a point where I feel confident, where I love myself and feel happy. And I think that reflects in the music. What do I hope people take away from it? Not to live their life for other people, accept who they are and be unafraid to show people who that is. I hope people feel the strength to fully be themselves, do what they want to do, and get out of relationships they feel unhappy in.”`;
     onResizeClipOverflowingText(description);
 
     descriptionButtonInteractions();
+
+    handleDescriptionMoreText(description);
 }
 
 import projectContainer from '../html/projectViewContainer.html?raw';
@@ -18,6 +43,29 @@ function loadContainer() {
         const regex = new RegExp(`\\{${placeholder}\\}`, 'g');
         replacedContent = replacedContent.replace(regex, value);
     }
+
+    const listOfThings = ['PROJECTviewMOREtitle', 'PROJECTviewMOREartist', 'PROJECTviewMOREyear'];
+
+    for (let i = 0; i < listOfThings.length; i++) {
+        const placeholder = listOfThings[i].toString();
+        const regex = new RegExp(`\\{${placeholder}\\}`, 'g');
+        let value = '';
+
+        if (placeholder === 'PROJECTviewMOREtitle') {
+            value = "UNHEALTHY (Deluxe)"
+        } else if (placeholder === 'PROJECTviewMOREartist') {
+            value = "Anne-Marie";
+        } else if (placeholder === 'PROJECTviewMOREyear') {
+            value = "2023";
+        } else if (placeholder === 'MOG_checkedDate') {
+            value = "checkedIndicator";
+        } else if (placeholder === 'MOGI_placeholder_itemID') {
+            value = "temporaryIidentifier";
+        }
+
+        replacedContent = replacedContent.replace(regex, value);
+    }
+
 
     document.getElementById(IDofElement).innerHTML = replacedContent;
     return;
@@ -107,6 +155,11 @@ function descriptionButtonInteractions() {
         console.log('Button was pressed!');
         // Perform any actions you want when the button is pressed
     });
+}
+
+function handleDescriptionMoreText(description) {
+    const descriptionP = document.getElementById("PROJECTviewMOREdescriptionP");
+    descriptionP.innerHTML = description;
 }
 
 
