434579c23a16c81a41a2abf80c5f1820351cdfba
Leo <lanker013@gmail.com>
2023-08-30 19:18:07 +0100

    UPDATE init.sh

--- deployment/init.sh
+++ deployment/init.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-echo "GitHub Repository Downloader and Extractor"
-echo "------------------------------------------"
-
-
 # Specify the GitHub repository URL and the specific folder you want to extract
 repo_url="https://github.com/LeoAnker0/Orange-Music-2/archive/main.zip"
-#target_folder="./deployment"
+target_folder="Orange-Music-2-main/deployment"
+
 # Download the zip archive of the repository
 wget $repo_url -O repo.zip
-
-# List the folders in the first level of the zip archive
-unzip -l repo.zip | awk 'BEGIN { FS="/" } NF > 3 { print $4 }' | sort | uniq
-
-# Ask the user to input the folder they want to extract
-read -p "Enter the folder you want to extract: " target_folder
 
 # Extract the specific folder from the downloaded zip archive
 unzip repo.zip "$target_folder/*" -d temp_folder
