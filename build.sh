64c666f1f1c45b6c86a8d31b70873560f7144bcc
Leo <lanker013@gmail.com>
2023-08-31 17:10:39 +0100

    UPDATE build.sh
    Have now made a build script, so that I only have to run one command to build the images, rather than many

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# build the arm version of the images and push them
+
+build_version="0.0.1"
+original_directory="$PWD"
+
+#get the latest version of the
+git pull
+
+# cd to the right directory
+
+cd ./src/web/Orange-Music-2
+
+docker buildx build --platform linux/arm64 -t leoanker/om2_frontend:"$build_version"-arm64 --push -f ./Dockerfile .
+
+cd ../../apis
+
+docker buildx build --platform linux/arm64 -t leoanker/om2_main:"$build_version"-arm64 --push -f ./Dockerfile.prod .
+
+cd ./chipmunk_processor
+
+docker buildx build --platform linux/arm64 -t leoanker/om2_chipmunk:"$build_version"-arm64 --push -f ./Dockerfile.prod .
+
+cd "$original_directory"
