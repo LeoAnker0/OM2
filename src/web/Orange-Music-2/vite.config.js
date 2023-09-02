bdb37dcef04a718a0ef42805d278b8fc941ad65e
Leo <lanker013@gmail.com>
2023-09-02 21:52:38 +0100

    UPDATE styling
    I have now started working on adding mobile mode, and that starts with fixing up the styling so that it now works on mobile, and well it has been pretty easy, and next up I will tackle the lcd/playback bar but that will hopefully be fairly painless as well, however I will be moving the things about, so that might bring some issues.

--- src/web/Orange-Music-2/vite.config.js
+++ src/web/Orange-Music-2/vite.config.js
@@ -1,14 +1,18 @@
 // vite.config.js
 
 import { defineConfig } from 'vite';
+import { resolve } from 'path'; // Import the 'resolve' function from Node.js
 
 export default defineConfig({
     // ... other config options ...
-
-    // Add the custom route for /signup
+    // Configure the server to handle custom routes
     server: {
-        routes: {
-
+        proxy: {
+            // You can define proxy rules for specific routes if needed
+            '/signup': {
+                target: resolve(__dirname, './signup/index.html'), // Use 'resolve' to get the absolute path
+                changeOrigin: true,
+            },
         },
     },
 });
